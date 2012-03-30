9b6e396e39d9b20f02a936feff1046433289b46a
Mitchell Cooper <cooper879@msn.com>
2012-03-30 15:58:19 -0400

    added base of executables

---
+++ lib/P.pm
@@ -0,0 +1,16 @@
+# Copyright (c) 2012, Mitchell Cooper
+package P;
+
+use warnings;
+use strict;
+use base 'JE';
+
+use JE;
+
+our $VERSION = 1.0;
+
+sub new {
+    my ($class, %opts) = @_;
+    my $self = $class->SUPER::new(%opts);
+}
+
