2355bbf79bc5fcbbe504e16683d1777fda07612f
Mitchell Cooper <cooper879@msn.com>
2012-03-30 16:03:53 -0400

    switch to running directory

--- bin/pje-interpreter.pl
+++ bin/pje-interpreter.pl
@@ -1,1 +1,11 @@
 #!/usr/bin/perl
+
+use warnings;
+use strict;
+
+our $dir;
+BEGIN {
+    chdir($dir = shift @ARGV or die "Run directory not specified.\n")
+    or die "Unable to switch working directory.\n";
+    unshift @INC, $dir;
+}
